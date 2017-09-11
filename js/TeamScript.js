81fb79a90140d22ba4a9e3e1ff4346f93e17a356
Dominic Brant <dombrant@gmail.com>
2017-09-10 19:24:31 -0700

    Separated fadeInOnScroll to each team member

--- js/TeamScript.js
+++ js/TeamScript.js
@@ -11,7 +11,19 @@ setInterval(function() {
     headerAnimation();
     fadeInOnScroll({
       triggerElement: teamMemberArray[0],
-      fadeInElements: teamMemberArray
+      fadeInElements: [teamMemberArray[0]]
+    });
+    fadeInOnScroll({
+      triggerElement: teamMemberArray[1],
+      fadeInElements: [teamMemberArray[1]]
+    });
+    fadeInOnScroll({
+      triggerElement: teamMemberArray[2],
+      fadeInElements: [teamMemberArray[2]]
+    });
+    fadeInOnScroll({
+      triggerElement: teamMemberArray[3],
+      fadeInElements: [teamMemberArray[3]]
     });
     fadeInEmailSignUp();
     didScroll = false;
